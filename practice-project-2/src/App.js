ca14537df6b35261cc009e550d398701d9ca3e73
thetrickuser <coolady1807@outlook.com>
2023-06-30 22:02:47 +0530

    practice projct 2 complete

---
+++ practice-project-2/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import InputForm from "./components/InputForm";
+import Results from "./components/Results";
+
+function App() {
+  const [userData, setUserData] = useState([]);
+  const [showResults, setShowResults] = useState(false);
+
+  const handleAddUser = (data) => {
+    setUserData((prevData) => [...prevData, data]);
+    setShowResults(true);
+  };
+
+  // if (userData === "") {
+  //   setShowResults(false);
+  // }
+
+  return (
+    <div>
+      <InputForm onAddUser={handleAddUser} />
+      {!showResults && <p>Please enter any data</p>}
+      {showResults && <Results data={userData} />}
+    </div>
+  );
+}
+
+export default App;
