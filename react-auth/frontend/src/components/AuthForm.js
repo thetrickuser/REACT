ef26c93e6abdcef806d920569d0806a530b86499
thetrickuser <coolady1807@outlook.com>
2023-08-26 09:54:46 +0530

    adding token expiration

--- react-auth/frontend/src/components/AuthForm.js
+++ react-auth/frontend/src/components/AuthForm.js
@@ -88,6 +88,9 @@ export async function action({ request, params }) {
   const token = responseData.token;
 
   localStorage.setItem("token", token);
+  const expiration = new Date();
+  expiration.setHours(expiration.getHours() + 1);
+  localStorage.setItem("expiration", expiration.toISOString());
 
   return redirect("/");
 }
--- react-auth/frontend/src/pages/Root.js
+++ react-auth/frontend/src/pages/Root.js
@@ -1,10 +1,36 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+  Outlet,
+  useLoaderData,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
 
-import MainNavigation from '../components/MainNavigation';
+import MainNavigation from "../components/MainNavigation";
+import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
 
+  const authToken = useLoaderData();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!authToken) {
+      return;
+    }
+
+    if (authToken === "EXPIRED") {
+      submit(null, { method: "post", action: "/logout" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
+    setTimeout(() => {
+      submit(null, { method: "post", action: "/logout" });
+    }, tokenDuration);
+  }, [authToken, submit]);
   return (
     <>
       <MainNavigation />
